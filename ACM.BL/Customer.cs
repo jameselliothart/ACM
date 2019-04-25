ff4787671d03c12feb90351f05ed9749b78d9959
jameselliothart <45054319+jameselliothart@users.noreply.github.com>
2019-04-24 21:33:44 -0500

    Handle only first or last name and add InstanceCount property

--- ACM.BL/Customer.cs
+++ ACM.BL/Customer.cs
@@ -12,9 +12,24 @@ namespace ACM.BL
         public string EmailAddress { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
+
         public string FullName
         {
-            get { return $"{LastName}, {FirstName}"; }
+            get
+            {
+                string fullName = LastName;
+                if (!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if (!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
         }
+
+        public static int InstanceCount { get; set; }
     }
 }
--- Tests/ACM.BLTest/CustomerTest.cs
+++ Tests/ACM.BLTest/CustomerTest.cs
@@ -24,5 +24,58 @@ namespace ACM.BLTest
             // Assert
             Assert.AreEqual(expected, actual);
         }
+
+        [TestMethod]
+        public void ShouldHandleFirstNameEmpty()
+        {
+            // Arrange
+            var customer = new Customer
+            {
+                LastName = "Bar"
+            };
+            string expected = "Bar";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ShouldHandleLastNameEmpty()
+        {
+            // Arrange
+            var customer = new Customer
+            {
+                FirstName = "Foo"
+            };
+            string expected = "Foo";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            // Arrange
+            var c1 = new Customer() { FirstName = "Ender" };
+            Customer.InstanceCount += 1;
+
+            var c2 = new Customer() { FirstName = "Bean" };
+            Customer.InstanceCount += 1;
+
+            var c3 = new Customer() { FirstName = "Peter" };
+            Customer.InstanceCount += 1;
+
+            // Act
+
+            // Assert
+            Assert.AreEqual(3, Customer.InstanceCount);
+        }
     }
 }

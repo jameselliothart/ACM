49326019500c787726fa9b3b0c33b587455468b7
jameselliothart <45054319+jameselliothart@users.noreply.github.com>
2019-04-27 15:46:16 -0500

    update order to have properties for customer and shippingaddress id's as well as list of orderitems

--- ACM.BL/Order.cs
+++ ACM.BL/Order.cs
@@ -8,18 +8,22 @@ namespace ACM.BL
 {
     public class Order
     {
-        public Order()
+        public Order() : this(0)
         {
 
         }
         public Order(int orderId)
         {
             OrderId = orderId;
+            OrderItems = new List<OrderItem>();
         }
 
         public int OrderId { get; private set; }
         public DateTimeOffset? OrderDate { get; set; }
-        
+        public int CustomerId { get; set; }
+        public int ShippingAddressId { get; set; }
+        public List<OrderItem> OrderItems { get; set; }
+
         /// <summary>
         /// Validates the order data
         /// </summary>
